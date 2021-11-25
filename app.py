c7673cd2e2cc2a552b620f1ff38c5d452b738500
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-25 15:39:12 +0300

    Initialization redirect to letter page after post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from secret import secretkey
 from flask_sqlalchemy import SQLAlchemy
 from backend import create_unique_id, add_letter_to_database, get_letter_id_from_db, get_letter_by_unique_letter_id
@@ -19,6 +19,7 @@ def create_letter():
         text = request.form.get('text')
         recipient = request.form.get('recipient')
         add_letter_to_database(letter_id, author, text, recipient)  # Add letter with all content in db
+        return redirect(f'http://127.0.0.1:5000/letter/{letter_id}')
     return render_template('index.html')
 
 
